94626386dc067d1212c689c6a4a4f4c0b7c3bc2b
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-09-02 07:36:26 +0300

    Add catalog-lead block

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "../header/Header"
 import Banner from "../banner/Banner";
+import CatalogLead from "../catalog-lead/CatalogLead";
 
 export default function App() {
   return (
-    <div className="container">
-      <Header />
-      <Banner />
+    <div className="page">
+      {/*<Banner/>*/}
+      <Header/>
+      <CatalogLead/>
     </div>
   );
 }
--- src/components/catalog-lead/CatalogLead.jsx
+++ src/components/catalog-lead/CatalogLead.jsx
@@ -4,26 +4,27 @@ import "./CatalogLead.scss";
 export default function CatalogLead() {
   return (
     <section className="catalog-lead">
-      <div className="catalog-lead__text-container">
-        <ul className="breadcrumbs">
-          <li className="breadcrumbs__item">
-            <a className="breadcrumbs__link"></a>
-          </li>
-          <li className="breadcrumbs__item">
-            <a className="breadcrumbs__link"></a>
-          </li>
-          <li className="breadcrumbs__item">
-            <a className="breadcrumbs__link"></a>
-          </li>
-        </ul>
-      </div>
-      <div className="catalog-lead__photo-container">
-        <img className="catalog-lead__photo"
-             src="/img/catalogLead-photo-1x.png"
-             srcSet="/img/catalogLead-photo-1x.png 1x, /img/catalogLead-photo-2x.png 2x"
-             width="299" height="260"
-             alt=""/>
-      </div>
+        <div className="catalog-lead__text-container">
+          <ul className="breadcrumbs">
+            <li className="breadcrumb">
+              <a className="breadcrumb__link">All categories</a>
+            </li>
+            <li className="breadcrumb">
+              <a className="breadcrumb__link">Woman</a>
+            </li>
+            <li className="breadcrumb">
+              <a className="breadcrumb__link breadcrumb__link--active">Jackets</a>
+            </li>
+          </ul>
+          <h1 className="catalog-lead__title">Denim Collection <br/> Fall 2019</h1>
+        </div>
+        <div className="catalog-lead__photo-container">
+          <img className="catalog-lead__photo"
+               src="/img/catalogLead-photo-1x.png"
+               srcSet="/img/catalogLead-photo-1x.png 1x, /img/catalogLead-photo-2x.png 2x"
+               width="299" height="260"
+               alt=""/>
+        </div>
     </section>
   );
 }
